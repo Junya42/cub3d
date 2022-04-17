bfd1f125e0aeb72c8835c538863e350ce57e9ac8
Junya <remiki.anis3@gmail.com>
2022-04-17 07:15:40 +0200

    map update

--- cub3d_bonus/srcs/init/init.c
+++ cub3d_bonus/srcs/init/init.c
@@ -6,19 +6,65 @@
 /*   By: anremiki <anremiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 03:11:42 by anremiki          #+#    #+#             */
-/*   Updated: 2022/04/17 06:45:28 by anremiki         ###   ########.fr       */
+/*   Updated: 2022/04/17 07:14:27 by anremiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+void	get_angle(t_cub *cub, char dir)
+{
+	if (dir == 'N')
+		cub->a = 4.71238;
+	else if (dir == 'E')
+		cub->a = 0;
+	else if (dir == 'S')
+		cub->a = PI / 2;
+	else if (dir == 'W')
+		cub->a = PI;
+	else
+		cub->a = 4.71238;
+}
+
+void    get_map_xy(char **map, t_cub *cub)
+{
+	int     y;
+	int     x;
+	int     max;
+
+	y = 0;
+	x = 0;
+	max = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (check_valid(map[y][x], "NSEW"))
+			{
+				cub->x = x * 64 + 32;
+				cub->y = y * 64 + 32;
+				get_angle(cub, map[y][x]);
+			}
+			x++;
+		}
+		if (x > max)
+			max = x;
+		y++;
+	}
+	cub->mx = max;
+	cub->my = y;
+}
 
 void	init_cub(t_cub *cub)
 {
 	t_player	player;
 	t_ray		ray;
 
+	get_map_xy(cub->map, cub);
 	player.dx = cos(cub->a) * 5;
 	player.dy = sin(cub->a) * 5;
+	player.angle = cub->a;
 	player.released = 0;
 	player.last_pressed = 0;
 	player.input_fix = 0;
@@ -81,6 +127,7 @@ int main(int ac, char **av)
 	}
 	cub.map = parse.map;
 	init_cub(&cub);
+	cub.exp = expand(cub.map, cub.mx, cub.my, 64);
 	create_window(&cub);
 	create_hooks(&cub);
 	mlx_loop_hook(cub.mlx, anti_ghosting, &cub);
