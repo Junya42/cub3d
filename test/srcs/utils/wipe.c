7fe97ae396939354d9e34662ea7ade94694476c4
Chafik MAROUF <cmarouf@e3r12p4.clusters.42paris.fr>
2022-05-10 14:50:08 +0200

    repush

--- test/srcs/utils/wipe.c
+++ test/srcs/utils/wipe.c
@@ -6,11 +6,26 @@
 /*   By: cmarouf <cmarouf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 20:23:45 by anremiki          #+#    #+#             */
-/*   Updated: 2022/05/10 14:41:12 by cmarouf          ###   ########.fr       */
+/*   Updated: 2022/05/10 14:47:02 by cmarouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+int	close_audio(t_cub *cub)
+{
+	int i;
+
+	i = -1;
+	while (++i < 4)
+		if (cub->foot_steps[i])
+			Mix_FreeChunk(cub->foot_steps[i]);
+	if (cub->light_aura)
+		Mix_FreeChunk(cub->light_aura);
+	Mix_CloseAudio();
+	SDL_Quit();
+	return (0);
+}
 
 void	free_sprites(t_cub *cub)
 {
@@ -35,14 +50,17 @@ void	free_sprites(t_cub *cub)
 
 int	wipe_data(t_cub *cub, t_parse *parse)
 {
+	close_audio(cub);
 	destroy_imgs(cub, cub->text);
 	free(cub->text);
+	free_data(parse);
 	free_array(cub->exp);
 	free(cub->ray);
 	free(cub->player);
 	free(cub->light);
 	free_sprites(cub);
-	close_game(cub, parse);
+	mlx_destroy_display(cub->mlx);
+	free(cub->mlx);
 	free_matrix(cub->chunk, cub->ey, cub->ex);
 	return (0);
 }
