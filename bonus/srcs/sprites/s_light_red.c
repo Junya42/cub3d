3270260cfb043b1869e69a90d45f28fd1ea4801e
Chafik MAROUF <cmarouf@e1r8p8.clusters.42paris.fr>
2022-05-25 18:08:06 +0200

    Added colors to the rest of the sprites

--- bonus/srcs/sprites/s_light_red.c
+++ bonus/srcs/sprites/s_light_red.c
@@ -6,7 +6,7 @@
 /*   By: cmarouf <cmarouf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 03:08:35 by cmarouf           #+#    #+#             */
-/*   Updated: 2022/05/23 17:55:28 by cmarouf          ###   ########.fr       */
+/*   Updated: 2022/05/25 17:14:32 by cmarouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int	check_animation_light_red(t_sp *s)
 	int	i;
 
 	i = -1;
-	s->anim_paths[0] = "./bonus/imgs/blue0.xpm";
-	s->anim_paths[1] = "./bonus/imgs/blue1.xpm";
-	s->anim_paths[2] = "./bonus/imgs/blue2.xpm";
-	s->anim_paths[3] = "./bonus/imgs/blue3.xpm";
-	s->anim_paths[4] = "./bonus/imgs/blue4.xpm";
-	s->anim_paths[5] = "./bonus/imgs/blue5.xpm";
-	s->anim_paths[6] = "./bonus/imgs/blue6.xpm";
-	s->anim_paths[7] = "./bonus/imgs/blue7.xpm";
+	s->anim_paths[0] = "./bonus/imgs/s_red/red0.xpm";
+	s->anim_paths[1] = "./bonus/imgs/s_red/red1.xpm";
+	s->anim_paths[2] = "./bonus/imgs/s_red/red2.xpm";
+	s->anim_paths[3] = "./bonus/imgs/s_red/red3.xpm";
+	s->anim_paths[4] = "./bonus/imgs/s_red/red4.xpm";
+	s->anim_paths[5] = "./bonus/imgs/s_red/red5.xpm";
+	s->anim_paths[6] = "./bonus/imgs/s_red/red6.xpm";
+	s->anim_paths[7] = "./bonus/imgs/s_red/red7.xpm";
 	while (++i < 8)
 		if (open(s->anim_paths[i], O_RDONLY) < 1)
 			return (0);
@@ -70,7 +70,7 @@ int	txt_light_red(t_sp *sp, t_cub *cub)
 		return (0);
 	if (init_animation(sp, cub) == 0)
 		return (0);
-	sp->img = mlx_xpm_file_to_image(cub->mlx, "./bonus/imgs/blue7.xpm",
+	sp->img = mlx_xpm_file_to_image(cub->mlx, "./bonus/imgs/s_red/red0.xpm",
 			&sp->a, &sp->b);
 	if (!sp->img)
 		return (0);
