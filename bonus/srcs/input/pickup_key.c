6e1dc8dce97ed354c5407a14dbf46dcd7a6b8276
Chafik MAROUF <cmarouf@e2r4p10.clusters.42paris.fr>
2022-05-31 15:53:31 +0200

    normed few things

--- bonus/srcs/input/pickup_key.c
+++ bonus/srcs/input/pickup_key.c
@@ -6,38 +6,38 @@
 /*   By: cmarouf <cmarouf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:28:53 by cmarouf           #+#    #+#             */
-/*   Updated: 2022/05/31 15:02:34 by cmarouf          ###   ########.fr       */
+/*   Updated: 2022/05/31 15:53:09 by cmarouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-int key_finding(t_parse *parse)
+int	key_finding(t_parse *parse)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    j = 0;
-    while (parse->floor[i])
-    {
-        j = 0;
-        while (parse->floor[i][j])
-        {
-            if (parse->floor[i][j] == 'K')
-                return (0);
-            j++;
-        }
-        i++;
-    }
-    return (1);
+	i = 0;
+	j = 0;
+	while (parse->floor[i])
+	{
+		j = 0;
+		while (parse->floor[i][j])
+		{
+			if (parse->floor[i][j] == 'K')
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
-void    pickup_key(t_cub *cub)
+void	pickup_key(t_cub *cub)
 {
-    if (cub->exit != 1)
-    {
-        cub->exit = 1;
-        Mix_PlayChannel(4, cub->take_key, 0);
-    }
+	if (cub->exit != 1)
+	{
+		cub->exit = 1;
+		Mix_PlayChannel(4, cub->take_key, 0);
+	}
 }
